4a589cb00aa5995b6a8d3014ce2ff7af1b1c81b5
Harvey <harvey@henghung.com>
2020-03-30 11:57:59 +0800

    增加marker顯示

--- src/components/Map/index.vue
+++ src/components/Map/index.vue
@@ -6,8 +6,35 @@
 
 <script>
 import L from 'leaflet';
-let map = null;
 const mapKey = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+let map = null;
+let mapController = {
+   addMark(item) {
+      let { geometry, properties } = item;
+      let [ lng, lat ] = geometry.coordinates;
+      let marker = L.marker([lat, lng]).addTo(map).bindPopup(this.renderHtml(properties));
+   },
+   renderHtml(data) {
+      const url = 'https://www.google.com.tw/maps/place/'
+      return `
+         <p class="siteName">${data.name}</p>
+         <p class="count">
+            口罩剩餘:
+            成人:${data.mask_adult}個 /
+            小孩:${data.mask_child}個
+         </p>  
+         <a href="${url + data.address}" target="_blank">地址:${data.address}</a>
+         <p class="phone">電話:${data.phone}</p>
+         <p class="updateTime">更新時間:${data.updated}</p>`
+   },
+   removeMarker() {
+      map.eachLayer((layer) => {
+         if (layer instanceof L.Marker) {
+            map.removeLayer(layer);
+         }
+      });
+   },
+}
 export default {
 	props: {
       pharmacyArr: {
@@ -28,7 +55,11 @@ export default {
          if (this.pharmacyArr.length === 0) return [];
          return this.pharmacyArr.filter(pharmacy => {
             let { county, town } = pharmacy.properties;
-            return county === this.city && town === this.district;
+            if (this.district !== '') {
+               return county === this.city && town === this.district;
+            } else {
+               return county === this.city;
+            }
          });
       }
    },
@@ -36,12 +67,23 @@ export default {
       initMap() {
          map = L.map('map', {
             center: [24.9875278, 121.3646047],
-            zoom: 11
+            zoom: 12
          });
          L.tileLayer(mapKey, {
             maxZoom: 18,
          }).addTo(map);
       },
+      addMark() {
+         this.currentData.forEach(item => {
+            mapController.addMark(item);
+         });
+      },
+   },
+   watch: {
+      currentData() {
+         mapController.removeMarker();
+         this.addMark();
+      }
    },
    mounted() {
       this.initMap();
@@ -53,5 +95,13 @@ export default {
 #map {
    width: 100%;
    height: 100vh;
+}
+
+.leaflet-popup-content > * {
+   font-size: 15px;
+}
+
+.leaflet-popup-content p {
+   margin: 5px 0;
 }
 </style>
